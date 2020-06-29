bfde51b31d8539794f4808aab2c90a9c6d9c74ee
Shruthi <sujichikku@gmail.com>
2020-06-29 08:35:26 +0530

    First Commit

---
+++ src/test/java/test/sample/newproject/FirstMavenExampleTest.java
@@ -0,0 +1,28 @@
+package test.sample.newproject;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.annotations.Test;
+
+
+public class FirstMavenExampleTest
+{
+	WebDriver driver;
+	
+	@Test
+	public void setUp()
+	{
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Hi\\Desktop\\Selenium\\chromedriver.exe");
+		driver=new ChromeDriver();
+		driver.get("https://www.amazon.in/");
+		WebElement accountAndList=driver.findElement(By.xpath("//a[@id='nav-link-accountList']//span[contains(text(),'Account & Lists')]"));
+		Actions ac = new Actions(driver);
+		ac.moveToElement(accountAndList).perform();
+		driver.findElement(By.xpath("//a[@class='nav-action-button']/span[text()='Sign in']")).click();
+		//driver.close();
+	}
+
+}
